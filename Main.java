3b7a6c7d651c4274f620871a0e289038c804ebde
Nahuel Urtasun <161488972+nahuel-urtasun@users.noreply.github.com>
2024-12-03 20:08:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-
-    public static Scanner sc = new Scanner(System.in);
-
+public class ListaCiudades {
     public static void main(String[] args) {
+        ArrayList<String> ciudades = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        String respuesta;
+
+        do {
+            System.out.println("Ingreese el nombre de la ciudad: ");
+            ciudades.add(scanner.nextLine());
+
+            System.out.println("Quiere agregar otra ciudad?");
+            respuesta = scanner.nextLine();
+
+        } while (respuesta.equalsIgnoreCase("si"));
+
+        System.out.println("Lista de ciudades ingresadas: ");
+        for (String c : ciudades) {
+            System.out.println(c);
+        }
+
+        scanner.close();
+
     }
 }
